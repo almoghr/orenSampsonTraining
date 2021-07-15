a44a2a4a5097c49be091228fc1bd51bc3f04be02
oren sampson <oren.sampson@gmail.com>
2021-07-15 21:21:44 +0300

    test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            this is my footer
-        </div>
-    )
-}
+  return <div>this is a footer</div>;
+};
 
-export default Footer
+export default Footer;
