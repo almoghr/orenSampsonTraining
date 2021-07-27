4bc6e05a3688d264aa6f181f023c2e2cc84918fb
oren sampson <oren.sampson@gmail.com>
2021-07-27 19:23:11 +0300

    redux devtools fix

--- src/store/reduxSaga/index.js
+++ src/store/reduxSaga/index.js
@@ -1,5 +1,6 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import productsSlice from "../reducers/products-slice";
 import { watcherSaga } from "./sagas/rootSaga";
@@ -10,7 +11,11 @@ const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [sagaMiddleware];
 
-const store = createStore(reducers, {}, applyMiddleware(...middleware));
+const store = createStore(
+  reducers,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 sagaMiddleware.run(watcherSaga);
 
