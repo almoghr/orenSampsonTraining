ae964def7b4f7127554498b21e05c94f83780364
oren sampson <oren.sampson@gmail.com>
2021-11-17 12:20:35 +0200

    layout refactoring

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,10 +1,9 @@
 import { Fragment } from "react";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import MessageDisplayer from "../MessageDisplayer/MessageDisplayer";
 import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import "react-toastify/dist/ReactToastify.css";
 import styles from "./Layout.module.scss";
 
 const Layout = (props) => {
@@ -13,7 +12,6 @@ const Layout = (props) => {
       <div className={styles.body}>
         <Header />
         <div className={styles.mainSite}>{props.children}</div>
-        <Footer />
       </div>
       <MessageDisplayer />
       <ToastContainer
